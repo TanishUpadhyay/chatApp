2148c855c0387a2d1979a3be1d02baf5f610af9d
Tanish <tanishdu13@gmail.com>
2023-12-09 23:18:10 +0530

    light-dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,35 @@
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
-import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from '@/components/providers/theme-provider';
 
-const font = Open_Sans({ subsets: ['latin'] })
+const font = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Discord App',
   description: 'Team Chat App',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
