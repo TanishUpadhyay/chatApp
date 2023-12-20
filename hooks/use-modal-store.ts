b6fb6632bbd1ed6bc78e49ff945081edb939b76f
Tanish <tanishdu13@gmail.com>
2023-12-20 12:39:18 +0530

    Edit & Delete Channels

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,10 +1,12 @@
 import { create } from "zustand";
-import { ChannelType, Server } from "@prisma/client";
+import { Channel, ChannelType, Server } from "@prisma/client";
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "deleteServer"|"leaveServer" ;
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "deleteServer"|"leaveServer"
+                          | "editChannel" | "deleteChannel"   ;
 
 interface ModalData {
   server?: Server,
+  channel?: Channel,
   channelType?: ChannelType;
 }
 interface ModalStore {
